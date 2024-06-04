5e7d7280f8eceeda6a0404ee82b813e00dddea80
Luiz Luchete <luizfernandolv@gmail.com>
2024-06-04 02:43:57 -0300

    fix: change email

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'suporte@oabnamedida.com.br'
+	const from = 'suporte@jusmemoriza.com.br'
 
 	const email = {
 		from,
