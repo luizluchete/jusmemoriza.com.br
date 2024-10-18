a7051ed9356c77a8bb26343359ba9d0207f20ebf
Luiz Luchete <luizfernandolv@gmail.com>
2024-10-18 19:24:14 -0300

    feat: initial implementation notes page

--- app/routes/_app+/flashcards+/comboId+/index.tsx
+++
@@ -1,6 +0,0 @@
-import { type LoaderFunctionArgs, redirect } from '@remix-run/node'
-
-export function loader({ params }: LoaderFunctionArgs) {
-	const comboId = params.comboId
-	return redirect(`/flashcards/${comboId}/type/initial`)
-}
--- app/routes/_app+/notes+/index.tsx
+++ app/routes/_app+/notes+/index.tsx
@@ -1,12 +1,32 @@
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { requireUserId } from '#app/utils/auth.server'
+import { prisma } from '#app/utils/db.server'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	return json({})
+	const userId = await requireUserId(request)
+	const notes = await prisma.noteUserQuiz.findMany({
+		select: { note: true, quizId: true },
+		where: { userId },
+	})
+	return json({ notes })
 }
 export default function Index() {
+	const { notes } = useLoaderData<typeof loader>()
 	return (
-		<div>
-			<h1>notes</h1>
+		<div className="flex w-full flex-col">
+			<h1 className="mb-5 text-center text-h5">Minhas anotações</h1>
+
+			<ul className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-4">
+				{notes.map(note => (
+					<li
+						key={note.quizId}
+						className="min-h-52 overflow-y-auto text-wrap rounded-lg border border-gray-200 p-2 text-justify shadow-md"
+					>
+						{note.note}
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
